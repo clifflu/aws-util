9996593e363d549254ff8e2bf5ed10089d77a016
Cliff Chao-kuan Lu <clifflu@gmail.com>
2013-09-02 09:00:55 +0000

    separate config & code & ui

--- conf/autoscaling/dev-web.py
+++ conf/autoscaling/dev-web.py
@@ -1,159 +1,74 @@
 # -*- coding: utf-8 -*-
+from lib import TS_ISO
 
-from lib import *
+CONF = {
+  'NAME': 'dev-web',
+  'REGION': 'us-west-1',
 
-from boto.ec2.autoscale import LaunchConfiguration
-from boto.ec2.autoscale import AutoScalingGroup
-from boto.ec2.autoscale import AutoScaleConnection
-from boto.ec2.autoscale import Tag
-from boto.ec2.autoscale import ScalingPolicy
-
-from boto.ec2.cloudwatch import connect_to_region
-from boto.ec2.cloudwatch import MetricAlarm
-
-conn = AutoScaleConnection()
-
-NAME = 'dev-web' # Name, General
-REGION = 'us-west-1'
-
-#
-# Launch Configuration
-#
-LC = {
-  'name': '%s_%s' % (NAME, TS_ISO),
-  'image_id': "ami-f686adb3",
-  'security_groups': ['sg-021eff6d'], # SG, ID only, managed elsewhere
-  'instance_type': 't1.micro',
-  'instance_monitoring': False,
-}
-
-lc = LaunchConfiguration(**LC)
-conn.create_launch_configuration(lc)
-
-#
-# Auto Scaling Group
-#
-ASG = {
-  'group_name': NAME, # Auto Scaling Group name
-  'load_balancers': [NAME], # ELB, managed elseware
-  'availability_zones': ['us-west-1a'],
-  'vpc_zone_identifier': 'subnet-7c0e7114',
-  'launch_config':lc,
-  'health_check_type': 'ELB',
-  'health_check_period': 60,
-  'default_cooldown': 60,
-  'min_size': 1,
-  'max_size': 12,
-}
-
-groups = conn.get_all_groups(names=[NAME])
-if (len(groups) > 0):
-  # update
-  asg = groups[0]
-  for k in ASG :
-    # asg not iterable, try-except to make sure asg[k] exists
-    try: asg.__getattribute__(k)
-    except: continue
-    asg.__setattr__(k, ASG[k])
-  asg.update()
-else:
-  #create
-  asg = AutoScalingGroup(**ASG)
-  conn.create_auto_scaling_group(asg)
-
-#
-# ASG Tags
-#
-ASG_TAGS = [
-  {
-    'key': 'Name',
-    'value': '[a] %s' % NAME,
-    'propagate_at_launch': True,
+  # Launch Configuration
+  'LC': {
+    'name': '%(name)s_' + TS_ISO,
+    'image_id': 'ami-6088a225',
+    'security_groups': ['sg-021eff6d'], # SG, ID only, managed elsewhere
+    'instance_type': 't1.micro',
+    'instance_monitoring': False,
   },
-  {
-    'key': 'serves',
-    'value': 'shared',
-    'propagate_at_launch': True,
+  # Auto Scaling Group
+  'ASG': {
+    'group_name': "%(name)s" , # Auto Scaling Group name
+    'load_balancers': ['dev-web'], # ELB, managed elseware
+    'availability_zones': ['us-west-1a'],
+    'vpc_zone_identifier': 'subnet-7c0e7114',
+    'health_check_type': 'ELB',
+    'health_check_period': 60,
+    'default_cooldown': 60,
+    'min_size': 1,
+    'max_size': 12,
   },
-  {
-    'key': 'stage',
-    'value': 'dev',
-    'propagate_at_launch': True,
-  },
-  {
-    'key': 'Role',
-    'value': 'web',
-    'propagate_at_launch': True,
-  },
-]
-tags = [
-    Tag(**dict(x.items() + [('resource_id', ASG['group_name'])])) for x in ASG_TAGS
-]
-conn.create_or_update_tags(tags)
-
-#
-# Triggers (Scaling Policy / Cloudwatch Alarm)
-#
-conn_cw = connect_to_region(REGION)
-alarm_dim = {'AutoScalingGroupName': ASG['group_name']}
-
-TRIGGERS = [
-  {
-    'policy': {
-      'name': "%s-down" % NAME, # Handles cloudwatch alarms of the same name
-      'as_name': NAME,
-      'adjustment_type': 'PercentChangeInCapacity',
-      'scaling_adjustment': '-25',
-      'cooldown': '60',
-      'min-adjustment-step': '1',
+  'ASG_TAGS': [
+    {'key': 'Name', 'value': '[a] %(name)s'},
+    {'key': 'serves', 'value': 'shared'},
+    {'key': 'stage', 'value': 'dev'},
+    {'key': 'Role', 'value': 'web'},
+  ],
+  'TRIGGERS': [
+    {
+      'policy': {
+        'name': "%(name)s-down", # Handles cloudwatch alarms of the same name
+        'adjustment_type': 'PercentChangeInCapacity',
+        'scaling_adjustment': '-25',
+        'cooldown': '60',
+        'min-adjustment-step': '1',
+      },
+      'alarm': {
+        'namespace': 'AWS/EC2',
+        'metric': 'CPUUtilization',
+        'statistic': 'Average',
+        'comparison' : '<=',
+        'threshold': '30',
+        'period': '60',
+        'evaluation_periods': '2',
+        'alarm_actions': None,
+      }
     },
-    'alarm': {
-      'name':'%s-down' % NAME,
-      'namespace': 'AWS/EC2',
-      'metric': 'CPUUtilization',
-      'statistic': 'Average',
-      'comparison' : '<=',
-      'threshold': '30',
-      'period': '60',
-      'evaluation_periods': '2',
-      'alarm_actions': None,
-      'dimensions': alarm_dim,
-    }
-  },
-  {
-    'policy': {
-      'name': "%s-up" % NAME, # Handles cloudwatch alarms of the same name
-      'as_name': NAME,
-      'adjustment_type': 'PercentChangeInCapacity',
-      'scaling_adjustment': '25',
-      'cooldown': '60',
-      'min-adjustment-step': '1',
+    {
+      'policy': {
+        'name': "%(name)s-up", # Handles cloudwatch alarms of the same name
+        'adjustment_type': 'PercentChangeInCapacity',
+        'scaling_adjustment': '25',
+        'cooldown': '60',
+        'min-adjustment-step': '1',
+      },
+      'alarm': {
+        'namespace': 'AWS/EC2',
+        'metric': 'CPUUtilization',
+        'statistic': 'Average',
+        'comparison' : '>=',
+        'threshold': '80',
+        'period': '60',
+        'evaluation_periods': '1',
+        'alarm_actions': None,
+      }
     },
-    'alarm': {
-      'name':'%s-up' % NAME,
-      'namespace': 'AWS/EC2',
-      'metric': 'CPUUtilization',
-      'statistic': 'Average',
-      'comparison' : '>=',
-      'threshold': '80',
-      'period': '60',
-      'evaluation_periods': '1',
-      'alarm_actions': None,
-      'dimensions': alarm_dim,
-    }
-  },
-]
-
-for T in TRIGGERS:
-  # Policies are safely overwritten, so not checked for existence
-  conn.create_scaling_policy(ScalingPolicy(**T['policy']))
-  policy = conn.get_all_policies(as_group=ASG['group_name'], policy_names=[T['policy']['name']])[0]
-
-  T['alarm']['alarm_actions'] = [policy.policy_arn]
-  hits = conn_cw.describe_alarms(alarm_names=[T['alarm']['name']])
-
-  conn_cw.create_alarm(MetricAlarm(**T['alarm']))
-
-
-
-
+  ], # Triggers
+}
--- lib/autoscaling.py
+++ lib/autoscaling.py
@@ -11,149 +11,82 @@ from boto.ec2.autoscale import ScalingPolicy
 from boto.ec2.cloudwatch import connect_to_region
 from boto.ec2.cloudwatch import MetricAlarm
 
-conn = AutoScaleConnection()
+out = {}
 
-NAME = 'dev-web' # Name, General
-REGION = 'us-west-1'
+def setup(CONF):
+  global out
 
-#
-# Launch Configuration
-#
-LC = {
-  'name': '%s_%s' % (NAME, TS_ISO),
-  'image_id': "ami-f686adb3",
-  'security_groups': ['sg-021eff6d'], # SG, ID only, managed elsewhere
-  'instance_type': 't1.micro',
-  'instance_monitoring': False,
-}
+  lookup_tbl = {
+    'name': CONF['NAME'],
+  }
 
-lc = LaunchConfiguration(**LC)
-conn.create_launch_configuration(lc)
+  conn = AutoScaleConnection()
 
-#
-# Auto Scaling Group
-#
-ASG = {
-  'group_name': NAME, # Auto Scaling Group name
-  'load_balancers': [NAME], # ELB, managed elseware
-  'availability_zones': ['us-west-1a'],
-  'vpc_zone_identifier': 'subnet-7c0e7114',
-  'launch_config':lc,
-  'health_check_type': 'ELB',
-  'health_check_period': 60,
-  'default_cooldown': 60,
-  'min_size': 1,
-  'max_size': 12,
-}
+  out['conn'] = conn
 
-groups = conn.get_all_groups(names=[NAME])
-if (len(groups) > 0):
-  # update
-  asg = groups[0]
-  for k in ASG :
-    # asg not iterable, try-except to make sure asg[k] exists
-    try: asg.__getattribute__(k)
-    except: continue
-    asg.__setattr__(k, ASG[k])
-  asg.update()
-else:
-  #create
-  asg = AutoScalingGroup(**ASG)
-  conn.create_auto_scaling_group(asg)
+  # Launch Configurations
+  LC = CONF['LC']
+  LC['name'] = LC['name'] % lookup_tbl
 
-#
-# ASG Tags
-#
-ASG_TAGS = [
-  {
-    'key': 'Name',
-    'value': '[a] %s' % NAME,
-    'propagate_at_launch': True,
-  },
-  {
-    'key': 'serves',
-    'value': 'shared',
-    'propagate_at_launch': True,
-  },
-  {
-    'key': 'stage',
-    'value': 'dev',
-    'propagate_at_launch': True,
-  },
-  {
-    'key': 'Role',
-    'value': 'web',
-    'propagate_at_launch': True,
-  },
-]
-tags = [
-    Tag(**dict(x.items() + [('resource_id', ASG['group_name'])])) for x in ASG_TAGS
-]
-conn.create_or_update_tags(tags)
+  lc = LaunchConfiguration(**LC)
+  conn.create_launch_configuration(lc)
+  out['lc'] = lc
 
-#
-# Triggers (Scaling Policy / Cloudwatch Alarm)
-#
-conn_cw = connect_to_region(REGION)
-alarm_dim = {'AutoScalingGroupName': ASG['group_name']}
+  # Auto Scaling Group
+  ASG = CONF['ASG']
+  ASG['group_name'] = ASG['group_name'] % lookup_tbl
+  ASG['launch_config'] = lc
 
-TRIGGERS = [
-  {
-    'policy': {
-      'name': "%s-down" % NAME, # Handles cloudwatch alarms of the same name
-      'as_name': NAME,
-      'adjustment_type': 'PercentChangeInCapacity',
-      'scaling_adjustment': '-25',
-      'cooldown': '60',
-      'min-adjustment-step': '1',
-    },
-    'alarm': {
-      'name':'%s-down' % NAME,
-      'namespace': 'AWS/EC2',
-      'metric': 'CPUUtilization',
-      'statistic': 'Average',
-      'comparison' : '<=',
-      'threshold': '30',
-      'period': '60',
-      'evaluation_periods': '2',
-      'alarm_actions': None,
-      'dimensions': alarm_dim,
-    }
-  },
-  {
-    'policy': {
-      'name': "%s-up" % NAME, # Handles cloudwatch alarms of the same name
-      'as_name': NAME,
-      'adjustment_type': 'PercentChangeInCapacity',
-      'scaling_adjustment': '25',
-      'cooldown': '60',
-      'min-adjustment-step': '1',
-    },
-    'alarm': {
-      'name':'%s-up' % NAME,
-      'namespace': 'AWS/EC2',
-      'metric': 'CPUUtilization',
-      'statistic': 'Average',
-      'comparison' : '>=',
-      'threshold': '80',
-      'period': '60',
-      'evaluation_periods': '1',
-      'alarm_actions': None,
-      'dimensions': alarm_dim,
-    }
-  },
-]
+  groups = conn.get_all_groups(names=[ASG['group_name']])
+  if (len(groups) > 0):
+    # update
+    asg = groups[0]
+    for k in ASG :
+      # asg not iterable, try-except to make sure asg[k] exists
+      try: asg.__getattribute__(k)
+      except: continue
+      asg.__setattr__(k, ASG[k])
+    asg.launch_config_name = LC['name']
+    asg.update()
+    out['asg'] = asg
+  else:
+    #create
+    asg = AutoScalingGroup(**ASG)
+    conn.create_auto_scaling_group(asg)
 
-for T in TRIGGERS:
-  # Policies are safely overwritten, so not checked for existence
-  conn.create_scaling_policy(ScalingPolicy(**T['policy']))
-  policy = conn.get_all_policies(as_group=ASG['group_name'], policy_names=[T['policy']['name']])[0]
+  # ASG Tags
+  ASG_TAGS = CONF['ASG_TAGS']
+  for i in ASG_TAGS:
+    if 'propagate_at_launch' not in i:
+      i['propagate_at_launch'] = True
+    i.key = i.key % lookup_tbl
+    i.value = i.value % lookup_tbl
 
-  T['alarm']['alarm_actions'] = [policy.policy_arn]
-  hits = conn_cw.describe_alarms(alarm_names=[T['alarm']['name']])
+  tags = [
+      Tag(**dict(x.items() + [('resource_id', ASG['group_name'])])) for x in ASG_TAGS
+  ]
+  conn.create_or_update_tags(tags)
 
-  conn_cw.create_alarm(MetricAlarm(**T['alarm']))
+  # Triggers (Scaling Policy / Cloudwatch Alarm)
+  conn_cw = connect_to_region(REGION)
 
+  TRIGGERS = CONF['TRIGGERS']
+  for T in TRIGGERS:
+    T['policy']['name'] = T['policy']['name'] % lookup_tbl
+    T['policy']['as_name'] = ASG['group_name']
+    T['alarm']['dimensions'] = {'AutoScalingGroupName': ASG['group_name']}
+    T['alarm']['alarm_actions'] = None
 
+    if 'name' in T['alarm']:
+      T['alarm']['name'] = T['alarm']['name'] % lookup_tbl
+    else:
+      T['alarm']['name'] = T['policy']['name']
 
+    # Policies are safely overwritten, so not checked for existence
+    conn.create_scaling_policy(ScalingPolicy(**T['policy']))
+    policy = conn.get_all_policies(as_group=ASG['group_name'], policy_names=[T['policy']['name']])[0]
 
+    T['alarm']['alarm_actions'] = [policy.policy_arn]
+    hits = conn_cw.describe_alarms(alarm_names=[T['alarm']['name']])
+
+    conn_cw.create_alarm(MetricAlarm(**T['alarm']))
